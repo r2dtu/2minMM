c2c06f1a0e60a6b2aac62b15f09cc1e230eddcc3
David <dctu@ucsd.edu>
2018-01-27 14:59:37 -0800

    - Added more maze tracking code (wall following) - More complete flood fill algorithm in C

--- MazeTracker_pseudocode.c
+++ MazeTracker_pseudocode.c
@@ -3,24 +3,55 @@
  * traversed boolean
  */
 struct maze_cell {
-  char t;
-  char r;
-  char b;
-  char l;
+  char n;
+  char e;
+  char s;
+  char w;
   bool visited;
 };
 
-struct maze_cell maze_cells[16][16];
+struct maze_cell maze_cells[16][16] = { 0 };
 
-struct maze_cell curr_cell;
+// NESW changes with mouse orientation
+char orientation = 0;
+char currX = 0;
+char currY = 0;
 
-/* Initialize curr_cell to first cell */
 /* Check current cell for walls */
 /* If detected a wall from any sensor, record it */
+if (detectNorthWall()) {
+  maze_cells[currX][currY].n = 1;
+
+  // Set top cell's south to 1
+  maze_cells[currX][currY + 1].s = 1;
+}
+
+if (detectEastWall()) {
+  maze_cells[currX][currY].e = 1;
+
+  // Set right cell's west to 1
+  maze_cells[currX + 1][currY].w = 1;
+}
+
+if (detectSouthWall()) {
+  maze_cells[currX][currY].s = 1;
+
+  // Set bottom cell's north to 1
+  maze_cells[currX][currY - 1].n = 1;
+}
+
+if (detectWestWall()) {
+  maze_cells[currX][currY].w = 1;
+
+  // Set left cell's east to 1
+  maze_cells[currX - 1][currY].e = 1;
+}
 
 /* Begin Depth-first search */
 /* If wall is not detected, go that direction. Priority: t, r, b, l */
+/* PRIORITY CHANGES WITH WHICH PARTITION THE MOUSE IS IN */
 /* Keep track of which forks to check using a priority-queue? */
+maze_cells[currX][currY].visited = true;
 
 /* Continue DFS until center of the maze is found */
 /* Once center of maze is found, proceed to floodfill */
--- floodfill.c
+++ floodfill.c
@@ -1,59 +1,139 @@
-const int nn=16;                //maximum size of array 
-short int a[nn+1][nn+1]; 		    //array which stores values of cells in maze 
-short int wall_stack[256][2]; 	//array for wallstack 
-short int wall_counter; 		    //counter for knowing position of elements in stack 
-short int check;			 	        //check short 
-int ff_stack[256][2];           //main stack
-int ff_count; 	                //main stack counter for knowing position 
+/*
+ *
+ */
 
-void flood() {                  //function  
-  int level=0,i;                //initialize level to 0
-  clear_wstack(); 	            //function to clear wall stack 
-  start: 
-  // Err check
-  if(ff_count<0)goto end; 
-  
-  /* Repeat until ff_stack is empty:
-    1. Remove a cell from ff_stack
-    2. If DistanceValue(cell) = 255 {
-      Let DistanceValue(cell) = level
-      Place all open neighbors of cell into wall stack
+const int size = 12; // maze size
+int maze[size][size]; // stores cell distance values from center
+
+int adj_wallstack[size * size][2] = { 0 }; // stores neighboring cells 
+int wall_counter; // counter for knowing position of elements in stack 
+
+int ff_stack[size * size][2]; // main stack
+int ff_count; // main stack counter for knowing position
+
+/*
+ * Clears the adjacent walls stack.
+ */
+void clr_wallstack() {
+  for (int i = 0; i < size * size; i++) {
+    for (int j = 0; j < 2; j++) {
+      adj_wallstack[i][j] = 0;
     }
-  */
-  while(ff_count>=0) {
-    if(a[ff_stack[ff_count][0]][ff_stack[ff_count][1]]==255) { 
-      // Let DistanceValue(cell) = level
-      a[ff_stack[ff_count][0]][ff_stack[ff_count][1]]=level; 
-      // Place all open neighbors of cell into wall stack
-      nbor(ff_stack[ff_count][0],ff_stack[ff_count][1],nn); 
-    } 
-    ff_stack[ff_count][0]=ff_stack[ff_count][1]=0; 
-    --ff_count; 
-  } 
+  }
+}
 
-  getch();  // Get character
-  
-  // Check if wallstack is empty
-  if(wall_counter!=0) { 
-    // If not empty, incrementlevel
-    level++; 
-    while(wall_counter>0) { 
-      // Increment ff_count (change position)
-      ++ff_count; 
+/*
+ * Initializes the maze grid values to the max distance.
+ */
+void init_maze() {
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+			maze[i][j] = size * size - 1;
+    }   
+  }
+}
 
-      // Let ff_stack = wall stack 
-      {ff_stack[ff_count][0]=wall_stack[wall_counter-1][0]; 
-      
-      // Initialize ff_stack
-      ff_stack[ff_count][1]=wall_stack[wall_counter-1][1]; 
-      wall_counter--;} 
+/*
+ * Stores the adjacent cells on the adj_wallstack.
+ */
+void addNeighbors(int x, int y) {
+
+  // Check 4 cells. If there's a wall there, don't add it.
+  if (maze_cells[x][y].n == 0) {
+    adj_wallstack[wall_counter][0] = x;
+    adj_wallstack[wall_counter][1] = y + 1;
+    wall_counter += 1;
+  }
+  if (maze_cells[x][y].e == 0) {
+    adj_wallstack[wall_counter][0] = x + 1;
+    adj_wallstack[wall_counter][1] = y;
+    wall_counter += 1;
+  }
+  if (maze_cells[x][y].s == 0) {
+    adj_wallstack[wall_counter][0] = x;
+    adj_wallstack[wall_counter][1] = y - 1;
+    wall_counter += 1;
+  }
+  if (maze_cells[x][y].w == 0) {
+    adj_wallstack[wall_counter][0] = x - 1;
+    adj_wallstack[wall_counter][1] = y;
+    wall_counter += 1;
+  }
+
+}
+
+/*
+ * Floodfill algorithm (no mouse movement).
+ */
+void floodfill() {
+
+  // Initialize maze grid distances
+  init_maze();
+
+  // Keep track of the distance from the center 
+  int level = 0;
+
+  // Begin floodfill
+  while (true) {
+
+    // Error check
+    if (ff_count < 0) {
+      break;
     }
-    // Go back to beginning
-    clrscr(); 
-    clear_wstack();
-    goto start; 
-  } 
-  end: 
-  ; 
-}			
-   
+
+    // Initialize the adjacent walls stack
+    clr_wallstack();
+
+    /* 
+       Repeat until ff_stack is empty:
+       1. Remove a cell from ff_stack
+       2. If DistanceValue(cell) = 255 {
+       Let DistanceValue(cell) = level
+       Place all open neighbors of cell into wall stack
+       }
+     */
+    while (ff_count >= 0) {
+
+      // Get the cell's x, y (from the stack)
+      int currX = ff_stack[ff_count][0];
+      int currY = ff_stack[ff_count][1];
+
+      // Update the distance of the cell
+      if (maze[currX][currY] == 255) {
+
+        // Let DistanceValue(cell) = level
+        a[currX][currY] = level;
+
+        // Place all open neighbors of cell into wall stack
+        addNeighbors(ff_stack[ff_count][0], ff_stack[ff_count][1]);
+      }
+
+      // We've finished with that cell, go to next one on the stack
+      ff_stack[ff_count][0] = ff_stack[ff_count][1] = 0;
+      --ff_count;
+    }
+
+    // Pause?
+
+    // Check if wallstack is empty
+    if (wall_counter != 0) {
+
+      // If not empty, incrementlevel
+      level++;
+      while (wall_counter > 0) {
+
+        // Increment ff_count (change position)
+        ++ff_count;
+
+        // Let ff_stack = wall stack
+        ff_stack[ff_count][0] = adj_wallstack[wall_counter - 1][0];
+        ff_stack[ff_count][1] = adj_wallstack[wall_counter - 1][1];
+        wall_counter--;
+      }
+
+      // Zero out the wallstack
+      clr_wallstack();
+    }
+  }
+}
+
