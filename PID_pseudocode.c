3c61b6bcb1156a0abb9620496f87b5602f0e7619
David <dctu@ucsd.edu>
2018-02-03 13:41:05 -0800

    More implementable PID control

--- PID_pseudocode.c
+++ PID_pseudocode.c
@@ -9,17 +9,29 @@ double error_prior = 0;
 double totalError = 0;
 unsigned long iteration_time = 0;
 
+// Will change depending on which way we want to turn
+float encoderSetPoint;
+float gyroSetPoint;
+float accelSetPoint;
+
 int PID_Control() {
   float correction;
+
+  // Drive straight example
   float encoderError = leftEncoder.getPulses() - rightEncoder.getPulses();
   float gyroError = gyro.read() - gyroSetPoint;
+  float accelError = accel.read() - accelSetPoint;
 
   /* Deal with both errors together, or separately? */
 
-  totalError = totalError + (error*iteration_time);
-  derivative = (error – error_prior)/iteration_time;
+  // Calculate total error for PID
+  totalError = totalError + (error * iteration_time);
+  derivative = (error – error_prior) / iteration_time;
+
+  // Calculate correction
   correction = Kp * error + Ki * totalError + Kd * derivative + bias;
 
+  // Record current error for next iteration
   error_prior = error;
 
   return correction;
